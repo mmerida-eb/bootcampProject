0cd3c7dd07ef0936716f29474908917207751164
Mario Merida <mmerida@eventbrite.com>
2023-01-19 17:25:22 -0300

    create main screen

---
+++ ProyectBootcampSwift/View/MainView.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ProyectBootcampSwift
+//
+//  Created by Mario Merida on 18/01/2023.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    @IBOutlet weak var characterTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func previusButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+    }
+}
+
